bca4c866c764755af5f02a44724cf88dec5c491a
João Antônio de Castro <joaoaccastro@gmail.com>
2020-05-30 20:58:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function calcular(){
-    var inputQuero = document.querySelector(".inputQuero:checked").value;
-    var inputMereco = document.querySelector(".inputQuero:checked").value;
-    var inputPreciso = document.querySelector(".inputQuero:checked").value;
-    var inputPosso = document.querySelector(".inputQuero:checked").value;
-    var inputDevo = document.querySelector(".inputQuero:checked").value;
+    let inputQuero = document.querySelector(".inputQuero:checked").value;
+    let inputMereco = document.querySelector(".inputQuero:checked").value;
+    let inputPreciso = document.querySelector(".inputQuero:checked").value;
+    let inputPosso = document.querySelector(".inputQuero:checked").value;
+    let inputDevo = document.querySelector(".inputQuero:checked").value;
 
     if (inputQuero == "Sim" && inputMereco == "Sim" && inputPreciso == "Sim" && inputPosso == "Sim" && inputDevo == "Sim"){
         paragraphResult.innerHTML = "Baseado nas suas respostas, você pode comprar este ítem que tanto deseja."
     } else {
-        paragraphResult.innerHTML = "Baseado nas suas respostas, parece que este ítem não é tão necessário, vá dar uma volta que já já a vontade de comprar passa."
+        paragraphResult.innerHTML = "Baseado nas suas respostas, parece que este ítem não é tão necessário assim, vá dar uma volta que já já a vontade de comprar passa."
     }
 }
