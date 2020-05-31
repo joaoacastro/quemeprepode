420cb6ac14c82f18ca791f760e4a32866dc0404d
João Antônio de Castro <joaoaccastro@gmail.com>
2020-05-30 21:05:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function calcular(){
     let inputQuero = document.querySelector(".inputQuero:checked").value;
-    let inputMereco = document.querySelector(".inputQuero:checked").value;
-    let inputPreciso = document.querySelector(".inputQuero:checked").value;
-    let inputPosso = document.querySelector(".inputQuero:checked").value;
-    let inputDevo = document.querySelector(".inputQuero:checked").value;
+    let inputMereco = document.querySelector(".inputMereco:checked").value;
+    let inputPreciso = document.querySelector(".inputPreciso:checked").value;
+    let inputPosso = document.querySelector(".inputPosso:checked").value;
+    let inputDevo = document.querySelector(".inputDevo:checked").value;
 
     if (inputQuero == "Sim" && inputMereco == "Sim" && inputPreciso == "Sim" && inputPosso == "Sim" && inputDevo == "Sim"){
         paragraphResult.innerHTML = "Baseado nas suas respostas, você pode comprar este ítem que tanto deseja."
